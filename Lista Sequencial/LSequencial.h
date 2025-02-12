ed39978632128ff8c7d25ab86540f27a0232f6ad
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-02-12 13:21:10 -0300

    new file:   Lista Sequencial/.vscode/settings.json modified:   Lista Sequencial/LSequencial.h modified:   Lista Sequencial/listaSequencial.c++ new file:   Lista Sequencial/output/LSequencial.exe new file:   Lista Sequencial/output/listaSequencial.exe

--- Lista Sequencial/LSequencial.h
+++ Lista Sequencial/LSequencial.h
@@ -1,15 +1,16 @@
+#ifndef LSEQUENCIAL_H
+#define LSEQUENCIAL_H
 #include <iostream>
 
-typedef class listaSequencial{
+class listaSequencial{
     private:
-        int lista[100];
+        int lista[100] = {0};
         int tamanhoAtual;
         int tamanhoMaximo;
 
     public:
         listaSequencial();
         ~listaSequencial();
-        void criarLista();
         bool listaVazia();
         bool listaCheia();
         int tamanhoLista();
@@ -24,3 +25,73 @@ listaSequencial::listaSequencial(){
     tamanhoMaximo = 100;
 }
 
+listaSequencial::~listaSequencial(){
+    tamanhoAtual = 0;
+    tamanhoMaximo = 0;
+}
+
+bool listaSequencial::listaVazia(){
+    if (tamanhoAtual == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool listaSequencial::listaCheia(){
+    if (tamanhoAtual == tamanhoMaximo){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int listaSequencial::tamanhoLista(){
+    return tamanhoAtual;
+}
+
+int listaSequencial::elementoLista(int posicao){
+    if (posicao >= 0 && posicao <= tamanhoAtual){
+        return lista[posicao];
+    }
+    else{
+        return -1;
+    }
+}
+
+void listaSequencial::modificarElemento(int posicao, int valor){
+    if (posicao >= 0 && posicao <= tamanhoAtual){
+        lista[posicao] = valor;
+    }
+}
+
+void listaSequencial::inserirElemento(int posicao, int valor){
+    if (posicao >= 0 && posicao <= tamanhoAtual){
+        if (tamanhoAtual < tamanhoMaximo){
+            for (int i = tamanhoAtual; i >= posicao; i--){
+                lista[(i+1)] = lista[i];
+            }
+            lista[(posicao+1)] = valor;
+            tamanhoAtual++;
+        }
+    }
+}
+
+int listaSequencial::removerElemento(int posicao){
+    if (posicao >= 0 && posicao <= tamanhoAtual){
+        int valor = lista[posicao];
+        for (int i = posicao; i < tamanhoAtual; i++){
+            lista[i] = lista[(i+1)];
+        }
+        tamanhoAtual--;
+        return valor;
+    }
+    else{
+        return -1;
+    }
+}
+
+
+#endif
