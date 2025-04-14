7b9247b961b6636907a773d270b76f9e04e70460
KaikyCastro <kaiky.r.castro@gmail.com>
2025-04-14 13:21:55 -0300

    Funcionando

--- Árvore-Binária-de-Busca/main.cpp
+++ Árvore-Binária-de-Busca/main.cpp
@@ -3,24 +3,63 @@
 
 using namespace std;
 
-int main() {
-    ArvoreBinariadeBusca arvore;
-    Aluno aluno1(123, "João");
-    Aluno aluno2(456, "Maria");
-    Aluno aluno3(789, "Pedro");
+int main(){
+    ArvoreBinariadeBusca Arvorealunos;
+    int opcao, ra, imp;
+    string nome;
+    bool busca = false;
 
-    arvore.inserir(aluno1);
-    arvore.inserir(aluno2);
-    arvore.inserir(aluno3);
+    do{
+        cout << "Digite 0 para parar o algoritmo!\n";
+        cout << "Digite 1 para inserir um elemento!\n";
+        cout << "Digite 2 para remover um elemento!\n";
+        cout << "Digite 3 para buscar um elemento!\n";
+        cout << "Digite 4 para imprimir a arvore!\n";
+        cin >> opcao;
 
-    cout << "Imprimindo em ordem:" << endl;
-    arvore.imprimirEmOrdem(arvore.obterRaiz());
-
-    cout << "Imprimindo pré-ordem:" << endl;
-    arvore.imprimirPreOrdem(arvore.obterRaiz());
-
-    cout << "Imprimindo pós-ordem:" << endl;
-    arvore.imprimirPosOrdem(arvore.obterRaiz());
+        if (opcao == 1){
+            cout << "Digite o nome do aluno:\n";
+            cin >> nome;
+            cout << "Digite o RA do aluno:\n";
+            cin >> ra;
+            Aluno aluno(ra, nome);
+        if (Arvorealunos.estaCheia()){
+            cout << "A Árvore esta cheia!\n";
+            cout << "Nao foi possivel inserir o elemento!\n";
+        } else{            
+            Arvorealunos.inserir(aluno);
+        }
+        } else if (opcao == 2){
+            cout << "Digite o RA do aluno a ser removido!\n";
+            cin >> ra;
+            Aluno aluno(ra, " ");
+            Arvorealunos.remover(aluno);
+        } else if (opcao == 3){
+            cout << "Digite o RA do aluno a ser buscado!\n";
+            cin >> ra;
+            Aluno aluno(ra, " ");
+            Arvorealunos.buscar(aluno, busca);
+            if (busca){
+                cout << "Elemento encontrado!\n";
+                cout << "Nome: " << aluno.getNome() << endl;
+                cout << "RA: " << aluno.getMatricula() << endl;
+            } else{
+                cout << "Elemento nao encontrado!\n";
+            }           
+        } else if (opcao == 4){
+            cout << "Digite 1 para fazer a impressao em pre ordem!\n";
+            cout << "Digite 2 para fazer a impressao em ordem!\n";
+            cout << "Digite 3 para fazer a impressao em pos ordem!\n";
+            cin >> imp;
+            if (imp == 1){
+                Arvorealunos.imprimirPreOrdem(Arvorealunos.obterRaiz());
+            } else if (imp == 2){
+                Arvorealunos.imprimirEmOrdem(Arvorealunos.obterRaiz());
+            } else{
+                Arvorealunos.imprimirPosOrdem(Arvorealunos.obterRaiz());
+            }                   
+        }
+    } while (opcao != 0);
 
     return 0;
 }
