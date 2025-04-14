378bba6be5cbedaf057bd60699b4251ee3e6762a
KaikyCastro <kaiky.r.castro@gmail.com>
2025-04-14 11:06:41 -0300

    Busca feita

--- Árvore-Binária-de-Busca/ArvoreBinariadeBusca.cpp
+++ Árvore-Binária-de-Busca/ArvoreBinariadeBusca.cpp
@@ -25,6 +25,9 @@ bool ArvoreBinariadeBusca::estaVazia() {
 }
 
 bool ArvoreBinariadeBusca::estaCheia() {
+    // Tenta alocar memória para um novo nó
+    // Se a alocação falhar, a árvore está cheia
+    // Se a alocação for bem-sucedida, libera a memória e retorna falso
     try {
         No* novoNo = new No;
         delete novoNo;
@@ -35,21 +38,32 @@ bool ArvoreBinariadeBusca::estaCheia() {
 }
 
 void ArvoreBinariadeBusca::inserir(Aluno aluno) {
-    
+
+    // Verifica se a árvore está cheia
     if(estaCheia()) {
+        //Se estiver cheia, retorna uma mensagem de erro
         cout << "A árvore está cheia. Não é possível inserir o aluno." << endl;
         return;
     } else {
+
+        //Se não, cria um novo nó e atribui a esse nov, caracteristicas de aluno passado pela função, e inicia seus ponteiros filho como NULL
         No* novoNo = new No;
         novoNo->aluno = aluno;
         novoNo->filhoesquerda = NULL;
         novoNo->filhodireita = NULL;
 
+        //Verifica se a raiz está vazia, se estiver, atribui o novo nó a raiz
         if (raiz == NULL) {
             raiz = novoNo;
         } else {
-            No* temp = raiz;
+
+            //Se a raiz não estiver vazia, percorre a árvore para encontrar a posição correta para inserir o novo nó
+            No* temp = raiz; //Aloca um nó temporário qque começa na raiz, para percorrer a árvore
             while(temp != NULL) {
+
+                //Se o novo nó for menor que a matricula do aluno, então vai pra esquerda, porém tem que verificar se existe um nó la.
+                //se não tiver, então atribui o novo nó a esquerda do nó temporário
+                //Se tiver, então tem que percorrer pra próxima posição a esquerda
                 if (aluno.getMatricula() < temp->aluno.getMatricula()) {
                     if (temp->filhoesquerda == NULL) {
                         temp->filhoesquerda = novoNo;
@@ -58,6 +72,8 @@ void ArvoreBinariadeBusca::inserir(Aluno aluno) {
                         temp = temp->filhoesquerda;
                     }
                 } else {
+                    //Se o novo nó for maior, então vai pra direita, novamente deve veriricar se existe, se não existir, basta atribuir
+                    //Caso exista, então deve percorrer o filho a direita.
                     if (temp->filhodireita == NULL) {
                         temp->filhodireita = novoNo;
                         break;
@@ -75,6 +91,31 @@ void ArvoreBinariadeBusca::remover(Aluno aluno) {
 }
 
 void ArvoreBinariadeBusca::buscar(Aluno& aluno, bool& encontrado) {
+
+
+    //Encotnrado inicializa com false e cria-se um nó temporário que começa na raiz
+    encontrado = false;
+    No* temp = raiz;
+
+    //Faz o nó temporário percorrer a árvore, verificando se o nó temporário é nulo
+    while(temp != NULL) {
+
+        //Se o nó temporário for menor que a matricula do aluno, então vai pra esquerda
+        if (aluno.getMatricula() < temp->aluno.getMatricula()) {
+            temp = temp->filhoesquerda;
+
+        //Se o nó temporário for maior que a matricula do aluno, então vai pra direita
+        } else if (aluno.getMatricula() > temp->aluno.getMatricula()) {
+            temp = temp->filhodireita;
+
+        //Se n for nenhum dos casos anteriores, então é o nó que estamos procurando
+        //Então retorna-se True e o aluno recebe o nó temporário
+        } else {
+            encontrado = true;
+            aluno = temp->aluno;
+            break;
+        }
+    }
 
 }
 
